ff99411ce61918c1b426337e07da3c600337a213
chathuranga836@gmail.com <chaturanga836@gmail.com>
2025-12-19 18:19:45 +0530

    entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,14 @@ set -e
 
 echo "Starting Ranger Usersync container..."
 
-SETUP_MARKER="/opt/ranger-usersync/.setup_done"
+USER_SYNC_HOME="/opt/ranger-usersync/usersync"
+SETUP_MARKER="${USER_SYNC_HOME}/.setup_done"
+
+cd /opt/ranger-usersync
 
 if [ "$(id -u)" = "0" ] && [ ! -f "$SETUP_MARKER" ]; then
   echo "Running Ranger Usersync setup (one-time)..."
+  cd "$USER_SYNC_HOME"
   ./setup.sh
   chown -R ranger:ranger /opt/ranger-usersync /var/run/ranger
   touch "$SETUP_MARKER"
@@ -15,6 +19,7 @@ fi
 echo "Starting Ranger Usersync service..."
 
 exec su -s /bin/bash ranger -c "
+  cd $USER_SYNC_HOME && \
   ./ranger-usersync-services.sh start && \
   tail -F /opt/ranger-usersync/logs/usersync.log
 "
