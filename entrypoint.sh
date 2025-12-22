149bb78abeb383a7886911a2c03013a68af548a9
chathuranga836@gmail.com <chaturanga836@gmail.com>
2025-12-22 11:33:32 +0530

    fx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -ex   # <-- show commands + fail
+set -ex
 
 export JAVA_HOME=/opt/java/openjdk
 export PATH=$JAVA_HOME/bin:$PATH
@@ -9,11 +9,11 @@ echo "Starting Ranger Usersync..."
 echo "[DEBUG] Java version:"
 java -version
 
-echo "[DEBUG] Hadoop version:"
-$HADOOP_HOME/bin/hadoop version || true
+# Hadoop CLI NOT required for usersync
+echo "[DEBUG] Hadoop CLI not required for Ranger Usersync"
 
 echo "[DEBUG] CLASSPATH:"
-echo $CLASSPATH
+echo "$CLASSPATH"
 
 rm -f /opt/ranger-usersync/run/usersync.pid || true
 
